905e5175af85ce7942b6ece45b7d44c9a0b7c6a0
Michael Beaton <beatonma@gmail.com>
2025-10-30 14:51:35 +0000

    Fixed tests for `parseMarkdown`

--- app/src/commonTest/kotlin/org/beatonma/gclocks/util/MarkdownTest.kt
+++ app/src/commonTest/kotlin/org/beatonma/gclocks/util/MarkdownTest.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import org.beatonma.gclocks.app.theme.MarkdownTheme
-import org.beatonma.gclocks.test.shouldbe
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -21,40 +20,47 @@ private val theme = MarkdownTheme(
     italic = SpanStyle(fontStyle = FontStyle.Italic),
 )
 
-private infix fun AnnotatedString.shouldbe(other: AnnotatedString) {
-    assertEquals(other.text, this.text, message = "Text is not equal: ${this.text} != ${other.text}")
-    assertTrue(
-        this.hasEqualAnnotations(other),
-        message = "Annotations are not equal $this vs $other"
-    )
+private infix fun List<AnnotatedString>.shouldbe(other: List<AnnotatedString>) {
+    zip(other).forEach { (self, other) ->
+        assertEquals(other.text, self.text, message = "Text is not equal: ${self.text} != ${other.text}")
+        assertTrue(
+            self.hasEqualAnnotations(other),
+            message = "Annotations are not equal ${self} vs $other"
+        )
+    }
 }
+
+private fun build(block: AnnotatedString.Builder.() -> Unit) =
+    AnnotatedString.Builder().apply {
+        withStyle(theme.paragraph, block)
+    }.toAnnotatedString()
 
 
 class MarkdownTest {
     @Test
     fun `parseMarkdown is correct`() {
-        parseMarkdown("Hello", theme) shouldbe AnnotatedString("Hello")
-        parseMarkdown("*Hello*", theme) shouldbe AnnotatedString.Builder().apply {
+        parseMarkdown("Hello", theme) shouldbe listOf(build { append("Hello") })
+        parseMarkdown("*Hello*", theme) shouldbe listOf(build {
             withStyle(theme.italic) {
                 append("Hello")
             }
-        }.toAnnotatedString()
-        parseMarkdown("**Hello**", theme) shouldbe AnnotatedString.Builder().apply {
+        })
+        parseMarkdown("**Hello**", theme) shouldbe listOf(build {
             withStyle(theme.bold) {
                 append("Hello")
             }
-        }.toAnnotatedString()
+        })
 
-        parseMarkdown("# Hello", theme) shouldbe AnnotatedString.Builder().apply {
+        parseMarkdown("# Hello", theme) shouldbe listOf(build {
             withStyle(theme.h1) {
                 append("Hello")
             }
-        }.toAnnotatedString()
-        parseMarkdown("## Hello", theme) shouldbe AnnotatedString.Builder().apply {
+        })
+        parseMarkdown("## Hello", theme) shouldbe listOf(build {
             withStyle(theme.h2) {
                 append("Hello")
             }
-        }.toAnnotatedString()
+        })
 
         parseMarkdown(
             """# Heading
@@ -64,29 +70,32 @@ class MarkdownTest {
             |This is a paragraph with **bold** text and a [link](https://beatonma.org).
         """.trimMargin(),
             theme
-        ) shouldbe AnnotatedString.Builder().apply {
-            withStyle(theme.h1) {
-                append("Heading")
-            }
-            append("\n")
-            withStyle(theme.h2) {
-                append("This is a subheading")
-            }
-            append("\n")
-            append("This is a paragraph with ")
-            withStyle(theme.italic) {
-                append("italic")
-            }
-            append(" text.\n\n")
-            append("This is a paragraph with ")
-            withStyle(theme.bold) {
-                append("bold")
-            }
-            append(" text and a ")
-            withStyledLink("https://beatonma.org", theme.link) {
-                append("link")
-            }
-            append(".")
-        }.toAnnotatedString()
+        ) shouldbe listOf(
+            build {
+                withStyle(theme.h1) {
+                    append("Heading")
+                }
+                append("\n")
+                withStyle(theme.h2) {
+                    append("This is a subheading")
+                }
+                append("\n")
+                append("This is a paragraph with ")
+                withStyle(theme.italic) {
+                    append("italic")
+                }
+                append(" text.")
+            },
+            build {
+                append("This is a paragraph with ")
+                withStyle(theme.bold) {
+                    append("bold")
+                }
+                append(" text and a ")
+                withStyledLink("https://beatonma.org", theme.link) {
+                    append("link")
+                }
+                append(".")
+            })
     }
 }
