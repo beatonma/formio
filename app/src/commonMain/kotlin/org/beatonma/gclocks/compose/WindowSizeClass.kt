46d05b63ea94e3550ecc16884c8ade4d2c32d437
Michael Beaton <beatonma@gmail.com>
2025-11-05 11:16:48 +0000

    Dependencies update

--- app/src/commonMain/kotlin/org/beatonma/gclocks/compose/WindowSizeClass.kt
+++ app/src/commonMain/kotlin/org/beatonma/gclocks/compose/WindowSizeClass.kt
@@ -2,10 +2,6 @@ package org.beatonma.gclocks.compose
 
 import androidx.window.core.layout.WindowSizeClass
 
-fun WindowSizeClass.isWidthAtLeastExtraLarge() =
-    isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_EXTRA_LARGE_LOWER_BOUND)
-
-fun WindowSizeClass.isWidthAtLeastLarge() = isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_LARGE_LOWER_BOUND)
 fun WindowSizeClass.isWidthAtLeastExpanded() = isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND)
 fun WindowSizeClass.isWidthAtLeastMedium() = isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_MEDIUM_LOWER_BOUND)
 fun WindowSizeClass.isWidthSmall() = !isWidthAtLeastMedium()
